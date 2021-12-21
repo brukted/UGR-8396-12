80ce9cf7583cbfd760739f6ff69fcd1d0b4fe940
Bruk Tedla <biruk.tedla93@gmail.com>
2021-12-21 21:14:35 +0300

    V 1.1

--- js/script.js
+++ js/script.js
@@ -2,7 +2,5 @@ let hamburger = document.getElementById('nav-hamburger');
 let navList = document.getElementById('nav-list');
 
 hamburger.addEventListener('click', () => {
-    console.log('clicked');
-    console.log(navList.classList);
     navList.classList.toggle('nav__list--show');
 });
